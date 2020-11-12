539f99e69e1349e37b8e2b218b0968c99a25df0b
Ravi Stil <ravi.raaf@gmail.com>
2020-11-12 13:36:58 +0100

    Update

---
+++ js/script.js
@@ -0,0 +1,38 @@
+var mySwiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 50,
+    direction: 'horizontal',
+    loop: false,
+    navigation: {
+      nextEl: '.swiper-next',
+      prevEl: '.swiper-prev',
+    },
+});
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+  header: '.header'
+});
+
+var menuItems = document.querySelectorAll('.menu-item');
+var mobileOverlay = document.querySelector('.mobile');
+
+menuItems.forEach(link => {
+  link.addEventListener('click', (event) => {
+    mobileOverlay.classList.remove('active');
+  });
+});
+
+
+var menuOpen = document.querySelector('.menu-trigger-open');
+var menuClose = document.querySelector('.menu-trigger-close');
+
+function addOverlay() {
+  mobileOverlay.classList.add('active');
+}
+
+function removeOverlay() {
+  mobileOverlay.classList.remove('active');
+}
+
+menuOpen.addEventListener('click', addOverlay);
+menuClose.addEventListener('click', removeOverlay);
